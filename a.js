0bc4ee3b5ddbf343c5f70f6ab4746ee08ae1f3be
2theMoon <dlaguddh1@gmail.com>
2021-06-11 13:11:57 +0900

    css ex

--- a.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('P');
-
-btn.addEventListener("click", updateSign);
-
-function updateSign() {
-  let Sign = prompt("your name is...");
-  btn.textContent = "Don't touch me! " + Sign + "!!";
-}
